9266298b6e3bfdd14005b518a18ed8db68170fef
winter-fuyu <thpbirat@gmail.com>
2025-08-12 16:16:31 +0545

    fixes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,15 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ViteTypeOptions {
-  // By adding this line, you can make the type of ImportMetaEnv strict
-  // to disallow unknown keys.
-  strictImportMetaEnv: unknown;
+  strictImportMetaEnv: unknown; // to disallow unknown keys.
 }
 
 interface ImportMetaEnv {
   readonly VITE_CESIUM_TOKEN: string;
   readonly VITE_CESIUM_CONTAINER_ID: string;
-  // more env variables...
 }
 
 interface ImportMeta {
